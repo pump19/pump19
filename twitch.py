92e36be365c41a366fabb65f1e45d0b516a0e8a6
Twisted Pear <pear@twistedpear.at>
2015-01-11 08:32:38 +0000

    Wait for updater tasks to finish

---
+++ twitch.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8:ts=8:et:sw=4:sts=4:tw=79
+
+"""
+twitch.py
+
+File containing Twitch API utility functions.
+It sets up logging and provides coroutines for querying Twitch.tv APIs.
+
+Copyright (c) 2015 Twisted Pear <pear at twistedpear dot at>
+See the file LICENSE for copying permission.
+"""
+
+import asyncio
+import aiohttp
+import logging
+
+CHATTERS_URL = "http://tmi.twitch.tv/group/user/{stream}/chatters"
+BROADCAST_URL = ("https://api.twitch.tv/kraken/channels/"
+                 "{stream}/videos?limit={limit}&broadcasts=true")
+
+
+@asyncio.coroutine
+def get_broadcasts(stream, limit):
+    """
+    Request the latest n broadcasts for a given stream.
+    Returns an iterable of broadcasts, each entry being a tuple of title, url
+    and date.
+    """
+    logger = logging.getLogger("twitch")
+    logger.info("Requesting {limit} broadcast(s) for {stream}.".format(
+        stream=stream, limit=limit))
+
+    bc_url = BROADCAST_URL.format(stream=stream, limit=limit)
+    bc_req = yield from aiohttp.request(
+        "get", bc_url, headers={"Accept": "application/vnd.twitchtv.v3+json"})
+    broadcasts = yield from bc_req.json()
+
+    logger.debug("Retrieved {nof} broadcasts for {stream}.".format(
+        nof=len(broadcasts["videos"]), stream=stream))
+
+    return ((video["title"], video["url"], video["recorded_at"])
+            for video in broadcasts["videos"])
+
+
+@asyncio.coroutine
+def get_moderators(stream):
+    """
+    Request a list of moderators currently online in a given Twitch chat.
+    For convenience, the list will contain staff members, admins and global
+    moderators as well.
+    """
+    logger = logging.getLogger("twitch")
+    logger.info("Requesting chatters for {stream}.".format(stream=stream))
+
+    chatters_url = CHATTERS_URL.format(stream=stream)
+    chatters_req = yield from aiohttp.request(
+        "get", chatters_url, headers={"Accept": "application/json"})
+    chatters_dict = yield from chatters_req.json()
+    chatters = chatters_dict["chatters"]
+
+    moderators = chatters["moderators"]
+    moderators.extend(chatters["staff"])
+    moderators.extend(chatters["admins"])
+    moderators.extend(chatters["global_mods"])
+
+    logger.debug("Retrieved {nof} moderators for {stream}.".format(
+        nof=len(moderators), stream=stream))
+
+    return moderators
