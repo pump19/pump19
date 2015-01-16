b2fe5f559390d91dc56fe8324ccbf5336d2f8d9a
Twisted Pear <TwistedPear-AT@users.noreply.github.com>
2015-01-16 12:08:43 +0000

    Add new command routing and input validation

--- command.py
+++ command.py
@@ -13,14 +13,30 @@ See the file LICENSE for copying permission.
 import aiohttp
 import asyncio
 import bs4
-import database
 import functools
 import logging
+import re
 import twitch
 
-from sqlalchemy.sql import select, func as sqlfunc
+from psycopg2 import DataError
 
 PATREON_URL = "http://www.patreon.com/loadingreadyrun"
+
+CMD_REGEX = {
+    "patreon":
+        re.compile("patreon"),
+    "latest":
+        re.compile("latest(?: (?P<feed>video|podcast|broadcast))?"),
+    "quote":
+        re.compile("quote(?: (?:(?P<qid>\d+)|(?P<attrib>.+)))?"),
+    "addquote":
+        re.compile("addquote"
+                   "(?: \((?P<attrib_name>.+)\))?"
+                   "(?: \[(?P<attrib_date>\d{4}-[01]\d-[0-3]\d)\])?"
+                   "(?: (?P<quote>.+))"),
+    "delquote":
+        re.compile("delquote (?P<qid>\d+)")
+}
 
 
 class CommandHandler(object):
@@ -50,14 +66,47 @@ class CommandHandler(object):
                     yield from func(*args, **kwargs)
             return wrapper
 
-    def __init__(self, client, feed, *, prefix="!"):
+    class CommandRouter:
+        """A simple router matching strings against a set of regular
+           expressions match to a callable."""
+
+        routes = list()
+
+        def add_route(self, regex, callback):
+            self.routes.append((regex, callback))
+
+        def get_route(self, string):
+            for (regex, callback) in self.routes:
+                match = regex.match(string)
+                if not match:
+                    continue
+
+                # add matching groups to function
+                return functools.partial(callback, **match.groupdict())
+
+            return None
+
+    router = CommandRouter()
+
+    def __init__(self, client, feed, dbconn, *, prefix="!"):
         """Initialize the command handler and register for PRIVMSG events."""
         self.logger.info("Creating CommandHandler instance.")
 
         self.prefix = prefix
         self.client = client
         self.feed = feed
+        self.db = dbconn
         self.client.event_handler("PRIVMSG")(self.handle_privmsg)
+
+        self.setup_routing()
+
+    def setup_routing(self):
+        """Connect command handlers to regular expressions using the router."""
+        for key, regex in CMD_REGEX.items():
+            cmd_name = "handle_command_{0}".format(key)
+            handle_command = getattr(self, cmd_name, None)
+            if handle_command and callable(handle_command):
+                self.router.add_route(regex, handle_command)
 
     @asyncio.coroutine
     def handle_privmsg(self, nick, target, message):
@@ -65,29 +114,26 @@ class CommandHandler(object):
         Handle a PRIVMSG event and dispatch any command to the relevant method.
         """
         # ignore everything that's not a command with our prefix
-        if not message.startswith(self.prefix):
+        if not message.startswith(self.prefix) or len(message) < 2:
             return
 
-        # command is always separated by a space
-        parts = message.split(" ", 1)
-        cmd = parts[0]
-        args = parts[1].strip() if len(parts) == 2 else None
+        # remove prefix, regex will retrieve the arguments
+        cmd = message[1:]
 
-        self.logger.info("Got command {0} from {1}.".format(cmd, nick))
+        self.logger.info("Got command \"{0}\" from {1}.".format(cmd, nick))
 
         # is this a query? if so, send messages to nick instead
         if target == self.client.nickname:
             target = nick
 
         # check if we can handle that command
-        cmd_name = "handle_command_{0}".format(cmd[1:])
-        handle_command = getattr(self, cmd_name, None)
+        handle_command = self.router.get_route(cmd)
         if handle_command and callable(handle_command):
-            yield from handle_command(target, nick, args)
+            yield from handle_command(target, nick)
 
     @rate_limit()
     @asyncio.coroutine
-    def handle_command_patreon(self, target, nick, args):
+    def handle_command_patreon(self, target, nick):
         """
         Handle !patreon command.
         Post the number of patrons and the total earnings per month.
@@ -108,14 +154,13 @@ class CommandHandler(object):
 
     @rate_limit()
     @asyncio.coroutine
-    def handle_command_latest(self, target, nick, args):
+    def handle_command_latest(self, target, nick, *, feed=None):
         """
         Handle !latest [video|podcast|broadcast] command.
         Post the most recent RSS feed item or Twitch.tv broadcast.
         """
-        feed = "video"
-        if args and args in ["video", "podcast", "broadcast"]:
-            feed = args
+        if not feed:
+            feed = "video"
 
         # broadcasts are updated here
         if feed == "broadcast":
@@ -134,122 +179,113 @@ class CommandHandler(object):
 
     @rate_limit()
     @asyncio.coroutine
-    def handle_command_quote(self, target, nick, args):
+    def handle_command_quote(self, target, nick, *, qid=None, attrib=None):
         """
         Handle !quote [id] command.
         Post either the specified or a random quote.
         """
-        qid = int(args) if args and args.isdigit() else None
+        if qid:
+            qid = int(qid)
 
-        engine = yield from database.get_engine()
-        table = database.get_table("quote")
+        cur = yield from self.db.cursor()
+        if qid:
+            query = """SELECT qid, quote, attrib_name, attrib_date
+                        FROM quotes
+                        WHERE qid = %(qid)s
+                        LIMIT 1;"""
 
-        with (yield from engine) as conn:
-            query = table.select().limit(1)
+            yield from cur.execute(query, {"qid": qid})
+        elif attrib:
+            query = """SELECT qid, quote, attrib_name, attrib_date
+                        FROM quotes
+                        WHERE attrib_name ~~* %(attrib)s
+                        ORDER BY random()
+                        LIMIT 1;"""
+
+            search = "%{attrib}%".format(attrib=attrib)
+            yield from cur.execute(query, {"attrib": search})
+        else:
+            query = """SELECT qid, quote, attrib_name, attrib_date
+                        FROM quotes
+                        ORDER BY random()
+                        LIMIT 1;"""
+
+            yield from cur.execute(query)
+
+        if not cur.rowcount:
             if qid:
-                query = query.where(table.c.qid == qid)
+                no_quote_msg = "Could not retrieve quote #{qid}.".format(
+                    qid=qid)
+            elif attrib:
+                no_quote_msg = ("Could not retrieve quote "
+                                "matching \"{attrib}\".".format(attrib=attrib))
             else:
-                query = query.order_by(sqlfunc.random())
+                no_quote_msg = "Could not retrieve random quote."
 
-            res = yield from conn.execute(query)
-            if not res.rowcount:
-                if qid:
-                    no_quote_msg = "Could not retrieve quote #{qid}.".format(
-                        qid=qid)
-                else:
-                    no_quote_msg = "Could not retrieve random quote."
+            yield from self.client.privmsg(target, no_quote_msg)
+        else:
+            (qid, quote, name, date) = yield from cur.fetchone()
+            quote_msg = "Quote #{qid}: \"{quote}\"".format(qid=qid,
+                                                           quote=quote)
+            if name:
+                quote_msg += " ~{name}".format(name=name)
+            if date:
+                quote_msg += " [{date!s}]".format(date=date)
 
-                yield from self.client.privmsg(target, no_quote_msg)
-            else:
-                quote = yield from res.first()
-                quote_msg = "Quote #{qid}: {text}".format(
-                    qid=quote.qid, text=quote.text)
-
-                yield from self.client.privmsg(target, quote_msg)
+            yield from self.client.privmsg(target, quote_msg)
 
     @rate_limit()
     @asyncio.coroutine
-    def handle_command_addquote(self, target, nick, args):
+    def handle_command_addquote(self, target, nick, *, quote=None,
+                                attrib_name=None, attrib_date=None):
         """
-        Handle !addquote <quote> command.
+        Handle !addquote (<attrib_name>) [<attrib_date>] <quote> command.
         Add the provided quote to the database.
         Only moderators may add new quotes.
         """
-        if not args:
-            return
-        quote = args
-
-        if not (yield from twitch.is_moderator("loadingreadyrun", nick)):
+        if not quote:
             return
 
-        engine = yield from database.get_engine()
-        table = database.get_table("quote")
+        #if not (yield from twitch.is_moderator("loadingreadyrun", nick)):
+        #    return
 
-        with (yield from engine) as conn:
-            query = table.insert().values(text=quote)
-            yield from conn.execute(query)
+        cur = yield from self.db.cursor()
+        query = """INSERT INTO quotes (quote, attrib_name, attrib_date)
+                   VALUES (%(quote)s, %(attrib_name)s, %(attrib_date)s)
+                   RETURNING qid, quote, attrib_name, attrib_date;"""
+        try:
+            yield from cur.execute(query, {"quote": quote,
+                                           "attrib_name": attrib_name,
+                                           "attrib_date": attrib_date})
+        except DataError:
+            no_quote_msg = "Could not add quote."
+            yield from self.client.privmsg(target, no_quote_msg)
+        else:
+            (qid, quote, name, date) = yield from cur.fetchone()
+            quote_msg = "New quote #{qid}: \"{quote}\"".format(qid=qid,
+                                                               quote=quote)
+            if name:
+                quote_msg += " ~{name}".format(name=name)
+            if date:
+                quote_msg += " [{date!s}]".format(date=date)
 
-            # get the number of newly added quote
-            query = (select([table.c.qid]).order_by(table.c.qid.desc())
-                                          .limit(1))
-            query = table.select().order_by(table.c.qid.desc()).limit(1)
-            res = yield from conn.execute(query)
-            quote = yield from res.first()
-            new_quote_msg = "New quote #{qid}: {text}".format(
-                qid=quote.qid, text=quote.text)
-
-            yield from self.client.privmsg(target, new_quote_msg)
+            yield from self.client.privmsg(target, quote_msg)
 
     @rate_limit()
     @asyncio.coroutine
-    def handle_command_modquote(self, target, nick, args):
+    def handle_command_delquote(self, target, nick, *, qid=None):
         """
-        Handle !modquote <qid> <quote> command.
-        Modify the text for the provided quote ID.
-        Only moderators may modify quotes.
+        Handle !delquote <qid> command.
+        Delete the provided quote ID from the database.
+        Only moderators may delete quotes.
         """
-        if not args or len(args.split(" ", 1)) != 2:
-            return
-
-        qid, quote = args.split(" ", 1)
-        if not qid.isdigit():
+        if not qid or not qid.isdigit():
             return
         qid = int(qid)
 
         if not (yield from twitch.is_moderator("loadingreadyrun", nick)):
             return
 
-        engine = yield from database.get_engine()
-        table = database.get_table("quote")
-
-        with (yield from engine) as conn:
-            # make sure that qid really exists
-            query = table.select().where(table.c.qid == qid).alias().count()
-            count = yield from conn.scalar(query)
-            if not count:
-                return
-
-            query = table.update().where(table.c.qid == qid).values(text=quote)
-            yield from conn.execute(query)
-
-    @rate_limit()
-    @asyncio.coroutine
-    def handle_command_delquote(self, target, nick, args):
-        """
-        Handle !delquote <qid> command.
-        Delete the provided quote ID from the database.
-        Only moderators may delete quotes.
-        """
-        if not args or not args.isdigit():
-            return
-        qid = int(args)
-
-        if not (yield from twitch.is_moderator("loadingreadyrun", nick)):
-            return
-
-        engine = yield from database.get_engine()
-        table = database.get_table("quote")
-
-        with (yield from engine) as conn:
-            query = table.delete().where(table.c.qid == qid)
-            yield from conn.execute(query)
+        cur = yield from self.db.cursor()
+        query = "DELETE FROM quotes WHERE qid = %(qid)s;"
+        yield from cur.execute(query, {"qid": qid})
