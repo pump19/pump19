c138143f01511b81b86de46add0c7a1d697eeab4
Twisted Pear <TwistedPear-AT@users.noreply.github.com>
2015-05-03 06:55:45 +0000

    Add submodule for querying minecraft servers

---
+++ aiomc/aiomc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8:ts=8:et:sw=4:sts=4:tw=79
+
+import aiomc.protocol as protocol
+import asyncio
+import contextlib
+import json
+import logging
+
+"""
+aiomc.py
+
+Query Minecraft server information using asyncio.
+
+Copyright (c) 2015 Twisted Pear <pear at twistedpear dot at>
+See the file LICENSE for copying permission.
+"""
+
+logger = logging.getLogger("aiomc")
+logger.addHandler(logging.NullHandler())
+
+
+@asyncio.coroutine
+def get_status(host, port):
+    (rd, wr) = yield from asyncio.open_connection(host, port)
+    logger.info("Established connection to %s:%d", host, port)
+
+    packet = protocol.handshake(host, port)
+    wr.write(packet)
+    yield from wr.drain()
+
+    packet = protocol.status_request()
+    wr.write(packet)
+    yield from wr.drain()
+    length = yield from protocol.unpack_varint(rd)
+    logger.debug("Answer to status request is %d bytes long.", length)
+
+    # make sure to close the socket when we're done
+    with contextlib.closing(wr):
+        status = yield from protocol.unpack_varint(rd)
+        if status:
+            logger.error("Got error code %d for status request.", status)
+            return None
+
+        raw = yield from protocol.unpack_string(rd)
+        data = json.loads(raw)
+        return data
---
+++ aiomc/protocol.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8:ts=8:et:sw=4:sts=4:tw=79
+
+from asyncio import coroutine
+from struct import pack
+
+"""
+protocol.py
+
+A subset of the current Minecraft protocol.
+
+Copyright (c) 2015 Twisted Pear <pear at twistedpear dot at>
+See the file LICENSE for copying permission.
+"""
+
+
+def pack_varint(value):
+    packet = bytearray()
+    for _ in range(5):
+        if value & ~0x7F == 0:
+            packet += pack("B", value)
+            break
+        else:
+            packet += pack("B", value & 0x7F | 0x80)
+            value >>= 7
+    return bytes(packet)
+
+
+@coroutine
+def unpack_varint(stream):
+    result = 0
+    for i in range(5):
+        part = yield from stream.read(1)
+        part = ord(part)
+        result |= (part & 0x7F) << (7 * i)
+        if not part & 0x80:
+            return result
+    raise IOError("Could not parse data as VarInt.")
+
+
+def pack_string(value):
+    packet = bytearray()
+    payload = value.encode()
+    packet += pack_varint(len(payload))
+    packet += payload
+    return bytes(packet)
+
+
+@coroutine
+def unpack_string(stream):
+    length = yield from unpack_varint(stream)
+    raw = yield from stream.readexactly(length)
+    return raw.decode()
+
+
+def handshake(hostname, port, protocol=47):
+    payload = bytearray()
+    payload += pack_varint(0)  # packet ID
+    payload += pack_varint(protocol)
+    payload += pack_string(hostname)
+    payload += pack(">H", port)
+    payload += pack_varint(1)  # next state = Status
+
+    packet = bytearray()
+    packet += pack_varint(len(payload))
+    packet += payload
+    return bytes(packet)
+
+
+def status_request():
+    payload = bytearray()
+    payload += pack_varint(0)  # packet ID
+
+    packet = bytearray()
+    packet += pack_varint(len(payload))
+    packet += payload
+    return bytes(packet)
